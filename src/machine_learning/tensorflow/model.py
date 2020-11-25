e8e5655bc1e9b6058ad9250b78000b219e0e3343
Omochice-labsvr <h20ms419@hirosaki-u.ac.jp>
2020-11-25 19:29:51 +0900

    WIP

---
+++ src/machine_learning/tensorflow/model.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Conv2D, Concatenate, Dropout, Dense
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.utils import plot_model
+
+
+def construct_model(n_class: int):
+    input1 = Input(shape=(128, 128, 1))
+    change_channel = Conv2D(filters=3, kernel_size=3, padding="same",
+                            activation="relu")(input1)
+    mobilenet = MobileNetV2(include_top=False,
+                            input_shape=(128, 128, 3),
+                            weights="imagenet",
+                            pooling="max")(change_channel)
+    input2 = Input(shape=(32, ))
+    concate = Concatenate()([mobilenet, input2])
+    dense = Dense(128, activation="relu")(concate)
+    dropout = Dropout(0.3)(dense)
+    pred = Dense(n_class, activation="softmax")(dropout)
+    return tf.keras.Model(inputs=[input1, input2], outputs=pred)
+
+
+def show_model(model, dst: str = "model.png") -> None:
+    model.summary()
+    plot_model(model, to_file=dst)
+    print(f"model img saved in {dst}")
+
+
+if __name__ == "__main__":
+    pass
