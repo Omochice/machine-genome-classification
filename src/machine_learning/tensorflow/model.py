10f16c40926868338af838fdfe5d84d9a547ccb0
Omochice-labsvr <h20ms419@hirosaki-u.ac.jp>
2021-02-06 00:14:39 +0900

    update: change model

--- src/machine_learning/tensorflow/model.py
+++ src/machine_learning/tensorflow/model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv2D, Concatenate, Dropout, Dense
-from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.applications import MobileNetV2, MobileNet
 from tensorflow.keras.utils import plot_model
 
 
@@ -18,6 +18,7 @@ class MyModel(tf.keras.Model):
                                                           pooling="max")
 
 
+# Optunaでパラメータチューニングしたい
 def construct_model(n_class: int) -> tf.keras.Model:
     input1 = Input(shape=(192, 192, 1))
     change_channel = Conv2D(filters=3, kernel_size=3, padding="same",
@@ -28,9 +29,11 @@ def construct_model(n_class: int) -> tf.keras.Model:
                             pooling="max")(change_channel)
     input2 = Input(shape=(1, ))
     concate = Concatenate()([mobilenet, input2])
-    dense = Dense(128, activation="relu")(concate)
-    dropout = Dropout(0.3)(dense)
-    pred = Dense(n_class, activation="softmax")(dropout)
+    dense1 = Dense(512, activation="relu")(concate)
+    dropout1 = Dropout(0.3)(dense1)
+    dense2 = Dense(512, activation="relu")(dropout1)
+    dropout2 = Dropout(0.3)(dense2)
+    pred = Dense(n_class, activation="softmax")(dropout2)
     return tf.keras.Model(inputs=[input1, input2], outputs=pred)
 
 
