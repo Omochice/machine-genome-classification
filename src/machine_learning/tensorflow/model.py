5b8f0b49c57280f3af779d45080b6ffe044cd9fa
Omochice-labsvr <h20ms419@hirosaki-u.ac.jp>
2021-02-18 21:49:17 +0900

    update: update model inputs.
    入力を4種とれるようにした

--- src/machine_learning/tensorflow/model.py
+++ src/machine_learning/tensorflow/model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv2D, Concatenate, Dropout, Dense
-from tensorflow.keras.applications import MobileNetV2, MobileNet
+from tensorflow.keras.applications import MobileNetV2, MobileNet, MobileNetV3Small
 from tensorflow.keras.utils import plot_model
 
 
@@ -23,18 +23,20 @@ def construct_model(n_class: int) -> tf.keras.Model:
     input1 = Input(shape=(192, 192, 1))
     change_channel = Conv2D(filters=3, kernel_size=3, padding="same",
                             activation="relu")(input1)
-    mobilenet = MobileNetV2(include_top=False,
-                            input_shape=(192, 192, 3),
-                            weights="imagenet",
-                            pooling="max")(change_channel)
+    mobilenet = MobileNet(include_top=False,
+                          input_shape=(192, 192, 3),
+                          weights="imagenet",
+                          pooling="max")(change_channel)
     input2 = Input(shape=(1, ))
-    concate = Concatenate()([mobilenet, input2])
-    dense1 = Dense(512, activation="relu")(concate)
+    input3 = Input(shape=(4, ))
+    input4 = Input(shape=(1, ))
+    concate2 = Concatenate()([mobilenet, input2, input3, input4])
+    dense1 = Dense(512, activation="relu")(concate2)
     dropout1 = Dropout(0.3)(dense1)
     dense2 = Dense(512, activation="relu")(dropout1)
     dropout2 = Dropout(0.3)(dense2)
     pred = Dense(n_class, activation="softmax")(dropout2)
-    return tf.keras.Model(inputs=[input1, input2], outputs=pred)
+    return tf.keras.Model(inputs=[input1, input2, input3, input4], outputs=pred)
 
 
 def show_model(model, dst: str = "model.png") -> None:
